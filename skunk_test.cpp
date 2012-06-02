4e2456ce5b64102b10a98a6ba641ade572bbdc55
Karolina <karo@karo-N61Vg.(none)>
2012-06-02 15:40:57 +0200

    Opis funkcji pliku skunk_test.cpp

--- skunk_test.cpp
+++ skunk_test.cpp
@@ -2,52 +2,76 @@
 #include <string>
 #include <vector>
 
+
+
+/**
+ * @file skunk_test.cpp
+ * */
+
+
+
+/**
+ * Klasa dziedziczaca po TextField, umozliwia odczytywanie i ustawianie nowej
+ * wartosci pola tesktowego
+ * */
 class PoleTekstowe : public Skunk::TextField {
     std::string wartosc_;
 
 public:
-    PoleTekstowe(std::string w) : wartosc_(w) { }
+    PoleTekstowe(std::string w) : wartosc_(w) { }           
 
-    virtual std::string getValue() {
+    virtual std::string getValue() {                        /// odczytuje wartosc pola tekstowego
         return wartosc_;
     }
 
-    virtual void setValue(std::string& neu) {
+    virtual void setValue(std::string& neu) {               /// nadaje nowa wartosc polu tekstowemu
         wartosc_ = neu;
     }
 };
 
+
+/**
+ * Klasa umozliwiajaca nadawanie polu typu radio nowych wartosci
+ * */
 class PoleRadiowe : public Skunk::RadioButton {
 public:
     
-    virtual void setValue(int neu) {
+    virtual void setValue(int neu) {                        /// nadaje nowa wartosc polu typu radio
         index = neu;
     }
 
 };
 
+
+/**
+ * Klasa naglowka 
+ * */
 class WielkiNapis : public Skunk::Widget {
     virtual std::string GET() {
-        return "<h1><b>LOL WTF</b></h1>";
+        return "<h1><b>TIN HTTP APP</b></h1>";
     }
     virtual void POST(std::string&) { }
 };
 
+
+/**
+ * Main - tworzy nowy server z dostepnymi widgetami i uruchamia go
+ * */
 int main(void) {
     Skunk::Server *srv = new Skunk::Server();
     srv->addWidget(new WielkiNapis());
-    srv->addWidget(new PoleTekstowe("dupa"));
-    srv->addWidget(new PoleTekstowe("cycki"));
+    srv->addWidget(new PoleTekstowe("text1"));
+    srv->addWidget(new PoleTekstowe("text2"));
     PoleRadiowe *pr = new PoleRadiowe();
     srv->addWidget(pr);
-    pr->setTitle("Preferencje dupczane");
-    pr->addChoice("dupeczka");
-    pr->addChoice("dupcia");
-    pr->addChoice("dupiszon");
+    pr->setTitle("Preferencje");
+    pr->addChoice("Opcja1");
+    pr->addChoice("Opcja2");
+    pr->addChoice("Opcja3");
 
     Skunk::SimpleAuth *auth = new Skunk::SimpleAuth();
-    auth->addUser("admin", "dupa.8");
-    auth->addUser("user",  "dupa.8");
+    auth->addUser("admin", "admin.8");
+    auth->addUser("user",  "user.8");
 
     srv->setAuth(auth);
 
